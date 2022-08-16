6d4ef1843b247dfa934f667c2a3102bc0ccbc074
Aleksandr Shevkunenko <alex.4everyours@gmail.com>
2022-08-16 16:38:55 +0300

    CLI args parsed to settings

--- Makefile
+++ Makefile
@@ -1,18 +1,26 @@
 MAIN_DIR = ./cmd/dsync
-BINARY_NAME = dsync
+BINARY_NAME = dirsynchronizer
 
 help:
 	@echo "Possible targets:"
 	@echo "- help    for printing this help message"
-	@echo "- run     for building and running the project binary"
+	@echo "- run     for building and running the project binary in the background"
 	@echo "- build   for building the project"
+	@echo "- debug   for running the project binary in the foreground with some debug options turned on"
 	@echo "- clean   for cleaning the app main directory"
+	@echo "For the 'run' and 'debug' targets the variables 'srcdir' and 'copydir' are required, i.e. they set the directories for synchronization."
+	@echo "For example:"
+	@echo "make run srcdir=/path/to/source/dir copydir=/path/to/mirror/dir"
 
 build:
 	@cd ${MAIN_DIR} && go build -o ${BINARY_NAME} main.go
 
 run: build
-	@${MAIN_DIR}/${BINARY_NAME}
+	@${MAIN_DIR}/${BINARY_NAME} ${srcdir} ${copydir} &
+
+debug:
+	@cd ${MAIN_DIR} && go run -race main.go -log2std ${srcdir} ${copydir}
 
 clean:
 	@cd ${MAIN_DIR} && go clean
+	@rm -f ${MAIN_DIR}/${BINARY_NAME}
--- cmd/dsync/main.go
+++ cmd/dsync/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+type Settings struct {
+	srcDir   string
+	copyDir  string
+	logToStd bool
+	once     bool
+}
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("start")
+	settings, err := parseCommandArgs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+	fmt.Println("settings:", settings)
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("finish")
+}
+
+func parseCommandArgs() (*Settings, error) {
+	stg := new(Settings)
+
+	flagSet := flag.NewFlagSet("Directories Synchronizer CLI", flag.ExitOnError)
+	flagSet.BoolVar(&stg.logToStd, "log2std", false,
+		"if true, then logs are written to the console, otherwise - to the text file")
+	flagSet.BoolVar(&stg.once, "once", false,
+		"if true, then directories are synchronized only once (i.e. the program has finite execution), "+
+			"otherwise - the process is started and lasts indefinitely (until interruption)")
+	flagSet.Parse(os.Args[1:])
+
+	if flagSet.NArg() < 2 {
+		return nil, errors.New("at least two arguments (for the directories for synchronization) must present")
+	}
+	stg.srcDir, stg.copyDir = flagSet.Arg(0), flagSet.Arg(1)
+	if stg.srcDir == stg.copyDir {
+		return nil, errors.New("the directories for synchronization cannot be the same")
+	}
+	return stg, nil
 }

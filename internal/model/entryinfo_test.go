3d5e433ec2ea13147ad806071f43cf2c82d7fef1
Aleksandr Shevkunenko <alex.4everyours@gmail.com>
2022-10-03 09:32:23 +0300

    some more tests for EntryInfo in the model package

--- internal/model/entryinfo_test.go
+++ internal/model/entryinfo_test.go
@@ -84,3 +84,90 @@ func TestEntryInfo_IsSyncRequired(t *testing.T) {
 		})
 	}
 }
+
+func TestEntryInfo_ResolveOperationKind(t *testing.T) {
+	tests := []struct {
+		name  string
+		entry *EntryInfo
+		want  OperationKind
+	}{
+		{
+			name: "copy file",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: false},
+				CopyPathInfo: PathInfo{Exists: false},
+			},
+			want: OpKindCopyFile,
+		},
+		{
+			name: "copy dir",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: true},
+				CopyPathInfo: PathInfo{Exists: false},
+			},
+			want: OpKindCopyDir,
+		},
+		{
+			name: "remove file 1",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: false},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: false},
+			},
+			want: OpKindRemoveFile,
+		},
+		{
+			name: "remove file 2",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: true},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: false},
+			},
+			want: OpKindRemoveFile,
+		},
+		{
+			name: "remove dir",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: false},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: true},
+			},
+			want: OpKindRemoveDir,
+		},
+		{
+			name: "replace dir->file",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: false},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: true},
+			},
+			want: OpKindReplaceDirWithFile,
+		},
+		{
+			name: "replace file 1",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: false, Size: 10},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: false, Size: 20},
+			},
+			want: OpKindReplaceFile,
+		},
+		{
+			name: "replace file 2",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: false, Size: 10, ModTime: time.Unix(10000, 0)},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: false, Size: 10, ModTime: time.Unix(10100, 0)},
+			},
+			want: OpKindReplaceFile,
+		},
+		{
+			name: "same files",
+			entry: &EntryInfo{
+				SrcPathInfo:  PathInfo{Exists: true, IsDir: false, Size: 10, ModTime: time.Unix(10000, 0)},
+				CopyPathInfo: PathInfo{Exists: true, IsDir: false, Size: 10, ModTime: time.Unix(10000, 0)},
+			},
+			want: OpKindNone,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.want, tt.entry.ResolveOperationKind())
+		})
+	}
+}
